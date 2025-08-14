a49cfa2644718e616c7028fb7bed3a0e6f319615
seun <seun.edagolota@gmail.com>
2025-08-14 18:22:28 -0400

    react frontend

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import http from 'http';
 import websocket from 'websocket'
-import express from 'express'
-const app = express();
-app.get("/", (req,res)=> res.sendFile(__dirname, "/index.html"))
-app.listen(9091, () => console.log("Listening.. on 9091"))
+
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("Listening.. on 9090"))
 
@@ -43,6 +40,9 @@ wsServer.on("request", request => {
 
 })
 
-function generateRoomId(length = 6) {
-  return Math.random().toString(36).substr(2, length).toUpperCase();
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
+ 
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
